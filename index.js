d68edd83668495267d45dc7577b2c10f4a1d916d
H.Tatsuhiro <admin@h-tatsu.com>
2021-08-01 00:53:26 +0900

    Draw triangle by "THREE.Line"

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 window.addEventListener("DOMContentLoaded", init)
 
 function init() {
-    
+    const width = 1980, height = 1000;
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("#canvas")
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    const scene = new THREE.Scene();
+
+    // カメラを作成
+    const camera = new THREE.PerspectiveCamera(
+        45,
+        width / height,
+        1,
+        10000
+    );
+    camera.position.set(0, 0, +1000);
+
+    // 箱を作成
+    const material = new THREE.LineBasicMaterial({
+        color: 0x990000
+    });
+
+    const points = [];
+    points.push(new THREE.Vector3(150, 0, 0));
+    points.push(new THREE.Vector3(0, 150, 0));
+    points.push(new THREE.Vector3(0, 0, 150));
+    points.push(new THREE.Vector3(150, 0, 0));
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const line = new THREE.Line( geometry, material);
+
+    scene.add(line);
+
+    // 平行光源
+    const directionalLight = new THREE.DirectionalLight(
+        0xffffff
+    );
+    directionalLight.position.set(1, 1, 1);
+    // シーンに追加
+    scene.add(directionalLight);
+
+    // 初回実行
+    renderer.render(scene, camera);
 }
