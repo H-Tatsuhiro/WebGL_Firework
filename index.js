bf5664c8506d817dc8dc03e7080486ace6275fef
H.Tatsuhiro <admin@h-tatsu.com>
2021-08-07 04:52:19 +0900

    Move Triangle (Left to Right, Right to Left)

--- index.js
+++ index.js
@@ -23,11 +23,13 @@ function init() {
         color: 0x990000
     });
 
-    const points = [];
+    let points = [];
     points.push(new THREE.Vector3(150, 0, 0));
     points.push(new THREE.Vector3(0, 150, 0));
-    points.push(new THREE.Vector3(0, 0, 150));
+    points.push(new THREE.Vector3(0, 0, 0))
     points.push(new THREE.Vector3(150, 0, 0));
+
+    const points_tmp = points;
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
@@ -54,6 +56,31 @@ function init() {
         renderer.setSize(width, height);
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
+        renderer.render(scene, camera);
+    }
+
+    let moveToRight = true;
+
+    frameUpdate();
+
+    function frameUpdate() {
+        requestAnimationFrame(frameUpdate);
+        if (points[2].x == 350) {
+            moveToRight = false;
+        } else if (points[2].x == -350) {
+            moveToRight = true;
+        }
+        for (let i = 0; i < points.length; i++) {
+            let diff = 0;
+            if (moveToRight) diff = 10;
+            else diff = -10;
+            points[i].x += diff;
+        }
+        const scene = new THREE.Scene();
+        scene.add(directionalLight);
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        const line = new THREE.Line(geometry, material);
+        scene.add(line);
         renderer.render(scene, camera);
     }
 }
